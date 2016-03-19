821499d568b0d9641adf58478eb0d44a880a1deb
nedimdz <nedim.dziho@wizard.ba>
2016-03-19 22:33:16 +0100

    Using diferent Item List Views

--- app/src/main/java/com/example/nedim/sunshine_my/app/ForecastAdapter.java
+++ app/src/main/java/com/example/nedim/sunshine_my/app/ForecastAdapter.java
@@ -19,15 +19,35 @@ public class ForecastAdapter extends CursorAdapter {
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
+    private static final int VIEW_TYPE_COUNT = 2;
+
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return VIEW_TYPE_COUNT;
+    }
 
     /*
         Remember that these views are reused as needed.
-     */
+    */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+        // Choose the layout type
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
+        if (viewType == VIEW_TYPE_TODAY) {
+            layoutId = R.layout.list_item_forecast_today;
+        } else if (viewType == VIEW_TYPE_FUTURE_DAY) {
+            layoutId = R.layout.list_item_forecast;
+        }
 
-        return view;
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
 
     /*
@@ -58,7 +78,7 @@ public class ForecastAdapter extends CursorAdapter {
 
         // Read user preference for metric or imperial temperature units
         boolean isMetric = Utility.isMetric(context);
-        
+
         // Read high temperature from cursor
         double high = cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP);
         TextView highView = (TextView) view.findViewById(R.id.list_item_high_textview);
